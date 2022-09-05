894f003e8fcfaa0fc1f1867bd35f790aa5b08582
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-05 22:16:53 +0300

    Change the constructor for album

--- music_album.rb
+++ music_album.rb
@@ -1,8 +1,10 @@
-class Music_Album
-  attr_reader :spotify, :archived
+require './item'
 
-  def initialize
-    @spotify = false
+class Music_Album
+  attr_reader :on_spotify, :archived
+
+  def initialize(_on_spotify, _archived)
+    @on_spotify = false
     @archived = false
   end
 end
