bbcb6f350d7b1a7f4414941d5870870f3770877f
divine <divinemaina@gmail.com>
2022-09-05 17:48:16 +0200

    add team member 4 classes and methods as required

--- movie.rb
+++ movie.rb
@@ -1,12 +1,14 @@
 class Movie < Item
-    attr_accessor :id, :name, :publish_date, :silet
-    def initialize(name, publish_date, silet)
-        super(publish_date)
-        @id = Random.rand(1...1000)
-        @name = name
-        @silet = silet
-    end
-    def can_be_archived?
-        super && @silet == true
-    end
+  attr_accessor :id, :name, :publish_date, :silet
+
+  def initialize(name, publish_date, silet)
+    super(publish_date)
+    @id = Random.rand(1...1000)
+    @name = name
+    @silet = silet
+  end
+
+  def can_be_archived?
+    super && @silet == true
+  end
 end
