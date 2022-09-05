3afa57784ab73b525b972b76eef6af1ef954947b
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-05 19:16:46 +0300

    Fix linters

--- modules/app.rb
+++ modules/app.rb
@@ -1,53 +1,51 @@
-module App
-  class Catalog
-    attr_reader :books, :music_albums, :movies, :games
+class Catalog
+  attr_reader :books, :music_albums, :movies, :games
 
-    def initialize
-      @books = []
-      @music_albums = []
-      @movies = []
-      @games = []
-    end
-
-    # call the methods you create for various classes
-    def operation(input)
-      case input
-
-      when 1
-        puts "\nList @books"
-
-      when 2
-        puts "\nList @music_albums"
-      when 3
-        puts "\nList @movies"
-      when 4
-        puts "\nList @games"
-      when 5
-        puts "\nList all genres"
-      when 6
-        puts "\nList all labels"
-      when 7
-        puts "\nList all authors"
-      when 8
-        puts "\nList all sources"
-      when 9
-        puts "\nAdd @Book"
-      when 10
-        puts "\nAdd @music album"
-      when 11
-        puts "\nAdd @movie"
-      when 12
-        puts "\nAdd @game"
-      else
-        puts input
-      end
-    end
+  def initialize
+    @books = []
+    @music_albums = []
+    @movies = []
+    @games = []
   end
 
-  def self.extract_input(range)
-    input = gets.chomp
-    return "\n'#{input}' is not a valid input.\nPlease try again" unless range.include?(input.to_i)
+  # call the methods you create for various classes
+  def operation(input)
+    case input
 
-    input.to_i
+    when 1
+      puts "\nList @books"
+
+    when 2
+      puts "\nList @music_albums"
+    when 3
+      puts "\nList @movies"
+    when 4
+      puts "\nList @games"
+    when 5
+      puts "\nList all genres"
+    when 6
+      puts "\nList all labels"
+    when 7
+      puts "\nList all authors"
+    when 8
+      puts "\nList all sources"
+    when 9
+      puts "\nAdd @Book"
+    when 10
+      puts "\nAdd @music album"
+    when 11
+      puts "\nAdd @movie"
+    when 12
+      puts "\nAdd @game"
+    else
+      puts input
+    end
   end
+end
+
+def self.extract_input(range)
+  input = gets.chomp
+  return "\n'#{input}' is not valid.\nPlease try again" unless range.include?(input.to_i)
+
+  input.to_i
 end
