1fe62f133f3375f0480fef1f69793e3d936c625c
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-05 17:49:40 +0300

    make items an empty array

--- genre.rb
+++ genre.rb
@@ -1,6 +1,6 @@
-class Genre < Item
-  attr_reader :id
-  attr_accessor :names, :items
+class Genre
+  attr_reader :id :items
+  attr_accessor :names
 
   def initialize(names)
     @id = rand(1..1000)
