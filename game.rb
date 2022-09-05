71c1145e0cbcd67d0abe613a24d23dc7b7622d28
Sanja <sanja1mandic@gmail.com>
2022-09-05 21:02:26 +0200

    add correct property name in attr_accessor

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 require './item'
 
 class Game < Item
-  attr_accessor :mulitiplayer, :last_
+  attr_accessor :mulitiplayer, :last_played_at
 
   def initialize(mulitiplayer, last_played_at, *args)
     @mulitiplayer = mulitiplayer
@@ -13,3 +13,6 @@ class Game < Item
     super && (Time.now.year - Date.parse(@last_played_at).year) > 2
   end
 end
+
+# game1 = Game.new(2, '2/2/2021', '2/2/2015')
+# p game1.can_be_archived?
