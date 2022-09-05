7169f40c21797d234e25813fd43c61fe9aadbf40
divine <divinemaina@gmail.com>
2022-09-05 17:06:33 +0200

    Create Source class with an association to the Item class

--- source.rb
+++ source.rb
@@ -1,3 +1,9 @@
 class Source
-    
+    attr_reader :id
+    attr_accessor :name, :item
+    def initialize(name)
+        @id = Random.rand(1...1000)
+        @name = name
+        @items = []
+    end
 end 
