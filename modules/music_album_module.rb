4bd3115d3e50bc0141c179c6f70ae5428bafe5f9
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-07 09:56:13 +0300

    Add options on spotify

--- modules/music_album_module.rb
+++ modules/music_album_module.rb
@@ -3,7 +3,7 @@ module MusicAlbums
     puts 'Music albums list is empty! Add a music album.' if @music_albums.empty?
     music_albums.each_with_index do |music_album, index|
       s = "Last played at: #{music_album.name} - published: #{music_album.publish_date}"
-      p "#{index})  On Spotify: #{music_album.on_spotify} - " + s
+      p ["#{index})  On Spotify: #{music_album.on_spotify} - " + s]
     end
   end
 
@@ -12,8 +12,17 @@ module MusicAlbums
     album_name = gets.chomp
     print 'Published date: '
     published = gets.chomp
-    print 'On Spotify: '
-    album_spotify = gets.chomp
+    print 'On Spotify? (y/n): '
+
+    album_spotify = gets.chomp.upcase
+    case album_spotify
+    when 'Y'
+      album_spotify = true
+    when 'N'
+      album_spotify = false
+    else
+      puts 'Invalid option. Please try again.'
+    end
     album = MusicAlbum.new(album_name, published, album_spotify)
     @music_albums << album
     puts "Music album #{album_name} created successfully."
