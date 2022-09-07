169b9b4fe02110bed7e51ae8d05b73cf3d437e53
Sanja <sanja1mandic@gmail.com>
2022-09-07 09:47:32 +0200

    Save author and game objects when app is exited and when appp loads

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -1,8 +1,9 @@
 module Authors
+  @authors = []
   def list_authors
-    authors = add_authors
-    puts 'Author list is empty! Add a author.' if authors.empty?
-    authors.each_with_index do |author, index|
+    @authors = add_authors
+    puts 'Author list is empty! Add a author.' if @authors.empty?
+    @authors.each_with_index do |author, index|
       p "#{index}) #{author.first_name} #{author.last_name}"
     end
   end
@@ -16,5 +17,23 @@ module Authors
       init << Author.new(first_name, last_name)
     end
     init
+  end
+
+  def save_authors
+    author_hash = []
+    return if @authors.nil?
+
+    @authors.each do |author|
+      author_hash << { first_name: author.first_name, last_name: author.last_name, id: author.id, items: author.items }
+      File.open('authors.json', 'w') { |f| f.puts author_hash.to_json }
+    end
+  end
+
+  def load_authors
+    author_file = File.exist?('./authors.json') ? File.read('./authors.json') : '[]'
+    author_h = JSON.parse(author_file)
+    author_h.each do |author|
+      @authors << Author.new(author['first_name'], author['last_name'])
+    end
   end
 end
